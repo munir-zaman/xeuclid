b5028d4a8f0535f6589991b9fb8689de48189e88
MunirZaman <bijli.begum.bd@gmail.com>
2021-09-10 14:07:30 +0600

    some minor changes

--- tikz_draw.py
+++ tikz_draw.py
@@ -17,11 +17,14 @@ def_preamble="""%tikz_draw
 \\usepackage{amsfonts}
 \\usepackage{amssymb}
 \\usepackage[left=2cm,right=2cm,top=2cm,bottom=2cm]{geometry}
-\\usepackage{tikzpicture}
+\\usepackage{tikz}
 %preamble
 """
 
 def_editor="vim"
+pdflatex_command='pdflatex -shell-escape'
+
+def_vector_config="blue, thick"
 
 
 class Tikz():
@@ -40,6 +43,10 @@ class Tikz():
     def write(self,text):
         write_to_file(self.file_name,text)
 
+    def read(self):
+        with open(self.file_name) as file:
+            print(file.read())
+
     def edit(self,editor=def_editor):
         os.system(f'{editor} {self.file_name}')
 
@@ -48,6 +55,37 @@ class Tikz():
 
     def end(self,env):
         self.write('\\end{'+env+'}')
+
+    def pdf(self):
+        os.system(f'{pdflatex_command} {self.file_name}')
+
+    def draw_axis(self,xy_range=[-5,5],tick_labels=False):
+        axis_code=f"""
+    %axis
+    \\draw[<->] ({xy_range[0]},0) -- ({xy_range[1]},0);
+    \\draw[<->] (0, {xy_range[0]}) -- (0, {xy_range[1]});\n"""
+
+        axis_ticks_code="""
+    %axis ticks
+    \\foreach \\x in {"""+f"""{xy_range[0]+1},{xy_range[0]+2},...,{xy_range[1]-2},{xy_range[1]-1}"""+"""}
+        \\draw (\\x,-2pt) -- (\\x,2pt);\n""" + """%\n
+    \\foreach \\x in {"""+f"""{xy_range[0]+1},{xy_range[0]+2},...,{xy_range[1]-2},{xy_range[1]-1}"""+"""}
+        \\draw (-2pt,\\x) -- (2pt,\\x);\n"""
+
+        #TODO
+        code=axis_code+axis_ticks_code
+        self.write(code)
+
+    def draw_vector(vector,start=(0,0), config=def_vector_config):
+        X,Y=row_vector(vector)
+
+        Config=f"[{config},->]" if (config!=None or config!='') else "[->]"
+        code=f"""
+        %vector [{X}, {Y}]
+        \\draw{Config} {str(tuple(start))} -- {str((X,Y))};
+
+        """
+        self.write(code)
 
 
 
