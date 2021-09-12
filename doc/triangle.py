669faadbb3d1e9bcf34b16060ec703457c8eb1cb
MunirZaman <bijli.begum.bd@gmail.com>
2021-09-12 13:06:47 +0600

    example usage files

---
+++ doc/triangle.py
@@ -0,0 +1,38 @@
+from euclid2 import *
+from tikz_draw import *
+
+A=col_vector([2,1])
+B=col_vector([-1,4])
+C=col_vector([-2,1])
+
+bisector1=angle_bisector(A,B,C)
+bisector2=angle_bisector(B,C,A)
+#angle bisector of angle ABC and angle BCA
+
+I=bisector1.intersection(bisector2)
+# intersection of bisector1 and bisector2
+# I is the incenter of trinagle ABC
+
+tikz=Tikz('triangle.tex')
+
+tikz.begin('document')
+tikz.begin('tikzpicture')
+
+tikz.draw_grid(xy_range=[-5,5])
+tikz.draw_axis()
+
+tikz.draw_angle(A,C,B, radius=0.3)
+tikz.draw_angle(A,B,C, radius=0.3)
+tikz.draw_angle(B,A,C, radius=0.3)
+
+tikz.draw_path(A,B,C,cycle=True)
+tikz.draw_path(I,A)
+tikz.draw_path(I,B)
+tikz.draw_path(I,C)
+
+tikz.draw_points(A,B,C,I)
+
+tikz.end('tikzpicture')
+tikz.end('document')
+
+tikz.pdf()
