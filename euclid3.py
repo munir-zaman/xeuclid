d9924973afa57e2b8091ffe32805cca3a7063f08
MunirZaman <bijli.begum.bd@gmail.com>
2021-10-05 20:20:04 +0600

    added new class `Plane`

--- euclid3.py
+++ euclid3.py
@@ -1,5 +1,8 @@
 from euclid.euclid_math import *
 from euclid.euclid2 import dist, mid, GObject, Line, Segment, Ray
+import numpy as np
+from numpy.core.numeric import isclose
+from numpy.lib.shape_base import row_stack
 
 
 class GObject3(GObject):
@@ -7,6 +10,38 @@ class GObject3(GObject):
     def __init__(self):
         pass
 
+def intersection_line3_line3(line1, line2):
+    dx, dy, dz = row_vector(line2.A - line1.A)
+    v1, v2, v3 = row_vector(line1.v)
+    u1, u2, u3 = row_vector(line2.v)
+    
+    D = np.array([[dx], [dy]])
+    C = np.array([[v1, -u1], 
+                  [v2, -u2]])
+    T = system(C, D)
+    t1, t2 = row_vector(T)
+    
+    if isclose(dz, v3*t1 - u3*t2):
+        out = line1(t1)
+    else:
+        out = None
+    return out
+
+def intersection_line3_segment3(line, segment):
+    int_=intersection_line3_line3(line, segment.line)
+    if not int_ is None:
+        out=int_ if (int_ in segment) else None
+    else:
+        out=None
+    return out
+
+def intersection_segment3_segment3(segment1, segment2):
+    int_=intersection_line3_line3(segment1.line, segment2.line)
+    if not isnone(int_):
+        out=int_ if (int_ in segment1) and (int_ in segment2) else None
+    else:
+        out=None
+    return out
 
 class Line3(Line, GObject3):
     def __init__(self, A, B):
@@ -83,3 +118,61 @@ class Segment3(Segment, GObject3):
     def __truediv__(self,value):
         return Segment3(self.A/value, self.B/value)
 
+
+def intersection_plane_plane_plane(plane1, plane2, plane3):
+    C = col_vector([plane1.c, plane2.c, plane3.c])
+    n1x, n1y, n1z = row_vector(plane1.n)
+    n2x, n2y, n2z = row_vector(plane2.n)
+    n3x, n3y, n3z = row_vector(plane3.n)
+
+    N = np.array([[n1x, n1y, n1z],
+                  [n2x, n2y, n2z],
+                  [n3x, n3y, n3z]])
+    
+    X = system(N, C)
+
+    return X
+
+
+def intersection_plane_plane(plane1, plane2):
+    v = col_vector(np.cross(row_vector(plane1.norm()), row_vector(plane2.norm())))
+    return None
+
+
+class Plane(GObject3):
+    def __init__(self, A, B, C):
+        self.A = A
+        self.B = B
+        self.C = C
+        self.u = self.B - self.A
+        self.v = self.C - self.A
+        self.n = self.norm()
+        self.c = np.dot(row_vector(self.n), row_vector(self.A))
+        self.type = "plane"
+
+    def __call__(self, s, t):
+        return (self.A + self.u*s + self.v*t)
+
+    def normst(self, s, t):
+        return (self.A + norm(self.u)*s + norm(self.v)*t)
+
+    def norm(self):
+        U, V = row_vector(self.u), row_vector(self.v)
+        N = col_vector(np.cross(U, V))
+        n = N/dist(col_vector([0, 0, 0]), N)
+        return n
+
+    def fxy(self, x, y):
+        n, c = self.n, self.c
+        nx, ny, nz = row_vector(n)
+        if not isclose(nz, 0):
+            z = (c - (nx*x + ny*y))/nz
+        elif isclose((c - (nx*x + ny*y)), 0):
+            z = 1
+        else:
+            z = None
+        return z
+
+
+def impl_param_plane(n, c):
+    pass
