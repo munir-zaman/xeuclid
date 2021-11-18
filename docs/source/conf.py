06aa7244478b943288efa972d46a8b47212d29a8
MunirZaman <65268304+MunirZaman@users.noreply.github.com>
2021-11-18 07:37:58 +0600

    Update conf.py
    Got rid of 3rd party etxns

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,9 @@ release = __version__
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    "sphinx_copybutton",
-    'sphinx_autodoc_typehints',
 ]
+#    "sphinx_copybutton",
+#    'sphinx_autodoc_typehints',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
